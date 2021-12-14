30181a41dbaf663ea634893c46f58b4ba03c253a
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2021-12-14 20:55:48 -0300

    feat: added a custom gui create project command

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -13,3 +13,14 @@ Cypress.Commands.add('logout', () => {
   cy.get('.qa-user-avatar').click()
   cy.contains('Sign out').click()
 })
+
+Cypress.Commands.add('gui_createProject', ({ name, description }) => {
+  cy.visit('projects/new')
+
+  cy.get('#project_name').type(name)
+  cy.get('#project_description').type(description)
+  
+  cy.get('.qa-initialize-with-readme-checkbox').check()
+
+  cy.contains('Create project').click()
+})
